ea041cd5bb3a1e055ead0b82b1758778f05cce8d
Damon Tung <guangzhen.dong@163.com>
2018-03-15 14:45:41 +0800

    blockchain init

---
+++ blockchain.py
@@ -0,0 +1,29 @@
+import hashlib
+import json
+from time import time
+
+
+class Blockchain(object):
+
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+    def new_block(self):
+        pass
+
+    def new_transactions(self, sender, recipient, amount):
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
+        })
+        return self.last_block['index'] + 1
+
+    @staticmethod
+    def hash(block):
+        pass
+
+    @property
+    def last_block(self):
+        pass
